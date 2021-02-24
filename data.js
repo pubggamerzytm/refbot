29d9d502cfa38767eff3571a447997d9b8e9f854
pubggamerzytm <70851685+pubggamerzytm@users.noreply.github.com>
2021-02-24 11:11:44 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 module.exports = {
-  token: 'bot token, string', // you can get it in @BotFather
+  token: '1448447302:AAFZMZG_pMh_mXU6zLMgtraBC72ocarDFcM, string', // you can get it in @BotFather
   mongoLink: 'link to MongoDB - database, string', // e.g. Mongo Atlas
-  channel: 'promoting channel`s id, string or number (begins with minus)', 
-  chanLink: 'invite link to chanel, string', // can get this in channel`s settings
-  paymentsChan: 'channel where to publish withdraw requests, string or number', // test chan
+  channel: '-1111111111, string or number (begins with minus)', 
+  chanLink: 'https://t.me/hoIderboy, string', // can get this in channel`s settings
+  paymentsChan: 'Holderbots, string or number', // test chan
   inviteLink: 't.me/${past here bot`s link}?start=params', // params - only user id or something extra
   admins: ['admins', 'ids'],
   kh: 'developer`s id for errors sending'
